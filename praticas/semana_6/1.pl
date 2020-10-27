02d4b90e16dad935b1ba2d3eacc421a6eb71b418
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-10-27 12:25:06 +0000

    tp5

--- praticas/semana_6/1.pl
+++ praticas/semana_6/1.pl
@@ -11,3 +11,11 @@ map2([C|R], F, [TC|CR]) :-
     function(F, [C, TC]),
     map2(R, F, CR).
 
+/*
+my_map(List, TermName, Result)
+my_map([], _, []).
+my_map([H | Tail], TermName, [Result|AuxResult]):-
+    Term =.. [TermName, H, Result],
+    Term,
+    my_map(Tail, TermName, AuxResult).
+*/
